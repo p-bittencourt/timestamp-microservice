96c1c96829db3d150f2a4c1afb1ca7e9c026890d
pedro <pedromonteirobittencourt@gmail.com>
2025-01-22 14:05:43 -0300

    feat: implement /api endpoint which returns the current time formatted

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,19 @@
 import { Application, Request, Response } from 'express';
 
 export const createRoutes = (app: Application) => {
-  // http://expressjs.com/en/starter/basic-routing.html
   app.get('/', function (req: Request, res: Response) {
     res.sendFile(__dirname + '/views/index.html');
   });
 
-  // your first API endpoint...
-  app.get('/api/hello', function (req: Request, res: Response) {
-    res.json({ greeting: 'hello API' });
+  app.get('/api', function (req: Request, res: Response) {
+    const formattedUtc = new Date().toUTCString();
+    const unixDate = Date.now().toString();
+    console.log('formattedUtc: ', typeof formattedUtc);
+    console.log('unixDate: ', typeof unixDate);
+    res.json({ unix: unixDate, utc: formattedUtc });
+  });
+
+  app.get('/api/:date', function (req: Request, res: Response) {
+    res.json(req.params);
   });
 };
