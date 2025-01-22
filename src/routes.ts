dd0de3cbe3ee08a08d5fd8aee9d7ddf9e9befe0b
pedro <pedromonteirobittencourt@gmail.com>
2025-01-22 16:22:07 -0300

    feat: implemented logic to validate the input and format the output

--- src/routes.ts
+++ src/routes.ts
@@ -17,25 +17,49 @@ export const createRoutes = (app: Application) => {
       return;
     }
 
-    // Here we're handling the case for date objects sent as parameters
-    const input = req.params.date;
-    let date = new Date(input);
-
-    if (date.toString() !== 'Invalid Date') {
-      res.json({ date });
+    const validInput = checkValidInput(req.params.date);
+    if (!validInput.valid) {
+      res.json({ error: 'Invalid date format' });
       return;
     }
 
-    // In case previous date was an Invalid Date object, we parseInt on the string to extract the UTC integer
-    const parsed = parseInt(input);
-    date = new Date(parsed);
-
-    if (date.toString() !== 'Invalid Date') {
-      res.json({ date });
-      return;
-    }
-
-    // If we get here it means input couldn't be formatted into a date
-    res.json({ error: 'Invalid date format' });
+    const output = formatOutput(req.params.date, validInput.type);
+    res.json(output);
   });
 };
+
+function checkValidInput(input: string): { valid: boolean; type?: string } {
+  // Here we're handling the case for date objects sent as parameters
+  let date = new Date(input);
+  if (date.toString() !== 'Invalid Date') {
+    return { valid: true, type: 'utc' };
+  }
+
+  // In case previous date was an Invalid Date object, we parseInt on the string to extract the UTC integer
+  const parsed = parseInt(input);
+  date = new Date(parsed);
+  if (date.toString() !== 'Invalid Date') {
+    return { valid: true, type: 'unix' };
+  }
+
+  // If we get here it means input couldn't be formatted into a date
+  return { valid: false };
+}
+
+function formatOutput(
+  input: string,
+  type?: string
+): { unix: string; utc: string } {
+  if (type === 'unix') {
+    const parsed = parseInt(input);
+    const utcDate = new Date(parsed);
+    return { unix: input, utc: utcDate.toUTCString() };
+  } else {
+    const date = new Date(input);
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate() + 1;
+    const unixDate = Date.UTC(year, month, day).toString();
+    return { unix: unixDate, utc: date.toUTCString() };
+  }
+}
